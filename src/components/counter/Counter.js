16e260d4efa7f46d8ddfe31223a21eadf39920dc
sujata <gunalesujata@gmail.com>
2020-09-10 17:14:31 +0530

    count & transitions

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -3,9 +3,29 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
   return (
-    <>
-      <h1>Counter Component</h1>
-    </>
+    <div className="container">
+      <h1>Counter</h1>
+      <p className="counter">{count}</p>
+      <div className="buttons">
+        <button
+          type="button"
+          onClick={() => setCount(count - 1)}
+          className="btn"
+        >
+          decrease
+        </button>
+        <button type="button" onClick={() => setCount(0)} className="btn">
+          reset
+        </button>
+        <button
+          type="button"
+          onClick={() => setCount(count + 1)}
+          className="btn"
+        >
+          increase
+        </button>
+      </div>
+    </div>
   );
 }
 
