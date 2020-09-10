b3da2e46eb10563a8b71b62b2616ada900f71f02
sujata <gunalesujata@gmail.com>
2020-09-10 16:50:17 +0530

    styling

---
+++ src/components/counter/Counter.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>Counter Component</h1>
+    </>
+  );
+}
+
+export default Counter;
