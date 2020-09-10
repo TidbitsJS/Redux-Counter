b13944f74a7ad6cf0cb4fda12c85bfad97474aec
sujata <gunalesujata@gmail.com>
2020-09-11 00:36:08 +0530

    modal reducer payload

--- src/components/modal/ModalReducer.js
+++ src/components/modal/ModalReducer.js
@@ -2,8 +2,27 @@ import { MODAL_OPEN, MODAL_CLOSE } from "../../config/Actions";
 
 const defaultState = {
   name: "Modal Reducer",
+  text: "Modal Text",
+  isOpen: false,
 };
 
 export default function modalReducer(state = defaultState, action) {
-  return state;
+  switch (action.type) {
+    case MODAL_OPEN:
+      return {
+        ...state,
+        isOpen: true,
+        name: action.payload.name,
+        text: action.payload.text,
+      };
+    case MODAL_CLOSE:
+      return {
+        ...state,
+        isOpen: false,
+        name: "Modal Reducer",
+        text: "Modal Text",
+      };
+    default:
+      return state;
+  }
 }
