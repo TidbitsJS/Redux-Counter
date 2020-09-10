415a140175375477c26d38bf5fd7fb4e009afe0c
sujata <gunalesujata@gmail.com>
2020-09-11 00:45:44 +0530

    modal dispatch

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,22 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Modal = (props) => {
-  const isOpen = true;
+import { connect } from "react-redux";
+import { MODAL_CLOSE } from "../../config/Actions";
 
+const Modal = ({ isOpen, name, text, close }) => {
   return (
     <div className={`modal-overlay ${isOpen ? `isModalOpen` : null} `}>
       <div className="modal-container">
-        <h2>Random Matrix</h2>
-        <p style={{ fontSize: "1.3rem", marginTop: "1rem" }}>
-          Randomness of random text
-        </p>
-        <button className="btn btn-primary">close</button>
+        <h2>{name}</h2>
+        <p style={{ fontSize: "1.3rem", marginTop: "1rem" }}>{text}</p>
+        <button onClick={close} className="btn btn-primary">
+          close
+        </button>
       </div>
     </div>
   );
 };
 
-Modal.propTypes = {};
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  close: PropTypes.func.isRequired,
+};
 
-export default Modal;
+const mapStateToProps = ({ modalState: { isOpen, name, text } }) => {
+  return { isOpen, name, text };
+};
+
+function mapDispatchToProps(dispatch) {
+  return { close: () => dispatch({ type: MODAL_CLOSE }) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
