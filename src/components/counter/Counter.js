4585e1d0ace1ad53d14930f153684a3e761ef373
sujata <gunalesujata@gmail.com>
2020-09-11 00:52:18 +0530

    counter & modal connection

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { DECREASE, INCREASE, RESET } from "../../config/Actions";
+import { DECREASE, INCREASE, RESET, MODAL_OPEN } from "../../config/Actions";
 
 function Counter(props) {
   return (
@@ -8,25 +8,13 @@ function Counter(props) {
       <h1>Counter</h1>
       <p className="counter">{props.count}</p>
       <div className="buttons">
-        <button
-          type="button"
-          onClick={() => props.dispatch({ type: DECREASE })}
-          className="btn"
-        >
+        <button type="button" onClick={props.decrease} className="btn">
           decrease
         </button>
-        <button
-          type="button"
-          onClick={() => props.dispatch({ type: RESET })}
-          className="btn"
-        >
+        <button type="button" onClick={props.reset} className="btn">
           reset
         </button>
-        <button
-          type="button"
-          onClick={() => props.dispatch({ type: INCREASE })}
-          className="btn"
-        >
+        <button type="button" onClick={props.increase} className="btn">
           increase
         </button>
       </div>
@@ -38,18 +26,23 @@ function mapStateToProps({ countState: { count, name } }) {
   return { count: count, name: name };
 }
 
-/*
-
 function mapDispatchToProps(dispatch, ownProps) {
-  console.log(ownProps)
+  console.log(ownProps);
 
   return {
     increase: () => dispatch({ type: INCREASE }),
     decrease: () => dispatch({ type: DECREASE }),
-    reset: () => dispatch({ type: RESET })
-  }
+    reset: () => {
+      dispatch({ type: RESET });
+      dispatch({
+        type: MODAL_OPEN,
+        payload: {
+          text: "Here I go , there I am. Looking up , up & up.",
+          name: "Random Soul",
+        },
+      });
+    },
+  };
 }
 
-*/
-
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
